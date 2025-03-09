459b2cbad0855105f2292446839a109902ccf5a9
KubangPawis <xanderfernandezperegrin@gmail.com>
2025-03-09 14:35:18 +0800

    feat: fetch current wiki page metadata

--- main.py
+++ main.py
@@ -3,6 +3,27 @@ import requests
 import time
 import random
 
+def get_page_details(page_url):
+    url_page_response = requests.get(page_url)
+
+    # Page Title, URL
+    wiki_page_soup = BeautifulSoup(url_page_response.text, 'lxml')
+    page_title = wiki_page_soup.title.text
+    print(f'Page Title: {page_title}')
+    print(f'Page URL: {url_page_response.url}')
+
+    # Page Latest Change - History
+    page_url_title = page_url.split('/')[-1]
+    url_history_response = requests.get(f'https://en.wikipedia.org/w/index.php?title={page_url_title}&action=history')
+    wiki_history_soup = BeautifulSoup(url_history_response.text, 'lxml')
+    history_content = wiki_history_soup.find('section', id='pagehistory')
+    latest_change_group = history_content.find('ul', class_='mw-contributions-list')
+    latest_change_instance = latest_change_group.find('li')
+    latest_date_container = latest_change_instance.find('bdi')
+    latest_date = latest_date_container.find('a', class_='mw-changeslist-date').text
+
+    print(f'Latest Date: {latest_date}')
+
 def filter_target_headings(target_headings):
     filtered_arr = []
     for heading in target_headings:
@@ -15,6 +36,9 @@ def main():
     start_url = 'https://en.wikipedia.org/wiki/Artificial_intelligence'
     start_url_response = requests.get(start_url)
     soup = BeautifulSoup(start_url_response.text, 'lxml')
+    
+    # Extract current page metadata
+    get_page_details(start_url)
 
     # Main content area
     wikipage_content = soup.find('div', id='mw-content-text')
